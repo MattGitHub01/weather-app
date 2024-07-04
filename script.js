6ab359588842e758f40666618ba9b819cf24099c
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-07-03 18:03:40 -0600

    Rebuild DOM and website functionality after data deletion

--- script.js
+++ script.js
@@ -1,18 +1,78 @@
-const container = document.querySelector('.container');
-
 async function getWeather() {
-    let input = prompt(`Enter City: `);
-    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=e5c796bf6dad4abf835194705240107&q=${input}&days=3&aqi=no&alerts=no`, {mode: 'cors'});
+    const container = document.querySelector('.container');
 
-    if (response.ok) {
-        const data = await response.json();
-        const forecasts = data.forecast.forecastday;
-        const dayOne = forecasts[0];
-        const dayTwo = forecasts[1];
-        const dayThree = forecasts[2];
+    const title = document.createElement('h1');
+    title.classList.add('title');
+    title.textContent = `Weather Checker`;
+    container.appendChild(title);
 
-        container.textContent = `The weather in ${data.location.name}, ${data.location.country} is ${data.current.condition.text}. The current temperature is ${data.current.temp_f} degrees. Three Day Forecast:\n [1]Date ${dayOne.date}:  Condition: ${dayOne.day.condition.text} High: ${dayOne.day.maxtemp_f} Low: ${dayOne.day.mintemp_f}\n [2]Date ${dayTwo.date}:  Condition: ${dayTwo.day.condition.text} High: ${dayTwo.day.maxtemp_f} Low: ${dayTwo.day.mintemp_f}\n [3]Date ${dayThree.date}:  Condition: ${dayThree.day.condition.text} High: ${dayThree.day.maxtemp_f} Low: ${dayThree.day.mintemp_f}`;
-    }
+    const inputDiv = document.createElement('div');
+    inputDiv.classList.add('input-div');
+    container.appendChild(inputDiv);
+
+    const inputBar = document.createElement('input');
+    inputBar.classList.add('input-bar');
+    inputDiv.appendChild(inputBar);
+
+    const inputBtn = document.createElement('button');
+    inputBtn.classList.add('input-btn');
+    inputBtn.textContent = `Search`;
+    inputDiv.appendChild(inputBtn);
+
+    const todaysForecast = document.createElement('div');
+    todaysForecast.classList.add('todays-forecast');
+    container.appendChild(todaysForecast);
+
+    const threeDayDiv = document.createElement('div');
+    threeDayDiv.classList.add('three-day-div');
+    container.appendChild(threeDayDiv);
+
+    const dayOneForecast = document.createElement('div');
+    dayOneForecast.classList.add('forecast-div');
+    threeDayDiv.appendChild(dayOneForecast);
+
+    const dayOneTxt = document.createElement('span');
+    dayOneForecast.appendChild(dayOneTxt);
+
+    const dayTwoForecast = document.createElement('div');
+    dayTwoForecast.classList.add('forecast-div');
+    threeDayDiv.appendChild(dayTwoForecast);
+
+    const dayTwoTxt = document.createElement('span');
+    dayTwoForecast.appendChild(dayTwoTxt);
+
+    const dayThreeForecast = document.createElement('div');
+    dayThreeForecast.classList.add('forecast-div');
+    threeDayDiv.appendChild(dayThreeForecast);
+
+    const dayThreeTxt = document.createElement('span');
+    dayThreeForecast.appendChild(dayThreeTxt);
+
+
+    inputBtn.addEventListener('click', async () => {
+        let input = inputBar.value;
+        const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=e5c796bf6dad4abf835194705240107&q=${input}&days=3&aqi=no&alerts=no`, {mode: 'cors'});
+
+        if (response.ok) {
+            const data = await response.json();
+            let forecasts = data.forecast.forecastday;
+            let dayOne = forecasts[0];
+            let dayTwo = forecasts[1];
+            let dayThree = forecasts[2];
+
+            todaysForecast.textContent = `${data.location.name}, ${data.location.country}\n Currently ${data.current.condition.text} with a temperature of ${data.current.temp_f} degrees Fahrenheit.`;
+
+            dayOneTxt.textContent = `Date: ${dayOne.date}\n Condition: ${dayOne.day.condition.text}\n High: ${dayOne.day.maxtemp_f} Fahrenheit\n Low: ${dayOne.day.mintemp_f} Fahrenheit`;
+
+            dayTwoTxt.textContent = `Date: ${dayTwo.date}\n Condition: ${dayTwo.day.condition.text}\n High: ${dayTwo.day.maxtemp_f} Fahrenheit\n Low: ${dayTwo.day.mintemp_f} Fahrenheit`;
+
+            dayThreeTxt.textContent = `Date: ${dayThree.date}\n Condition: ${dayThree.day.condition.text}\n High: ${dayThree.day.maxtemp_f} Fahrenheit\n Low: ${dayThree.day.mintemp_f} Fahrenheit`;
+            inputBar.textContent = ``;
+        } else {
+            throw new Error(`${input.status}`);
+        }
+        
+    });
 }
 
 getWeather()
